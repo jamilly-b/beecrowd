78d8886aa6e5aef36639850794007e2712dd0950
jamilly-b <jbm2@discente.ifpe.edu.br>
2024-05-28 11:25:20 -0300

    1221 - Primo Rápido

---
+++ 1221.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++){
+        int numero;
+        bool primo = true;
+        cin >> numero;
+        
+        for(int j = 2; j <= sqrt(numero); j++){
+            if(numero % j == 0){
+                cout << "Not Prime" << endl;
+                primo = false;
+                break;
+            }
+        }
+        
+        if(primo == true){
+            cout << "Prime" << endl;
+        }
+    }
+    return 0;
+}
